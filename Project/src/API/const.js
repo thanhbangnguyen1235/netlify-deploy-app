f27b7cf434f85075019034af9c63779c2b929867
thanhbangnguyen1235 <bangnguyen1235@gmail.com>
2021-12-04 17:31:00 +0700

    change url

--- Project/src/API/const.js
+++ Project/src/API/const.js
@@ -6,5 +6,5 @@ export const API_KEY = "?api_key=a2df3d1a7611194432bbdf1fc80540f2";
 export const API_KEY_ALT = "&api_key=a2df3d1a7611194432bbdf1fc80540f2";
 export const IMG_SIZE_XSMALL = "https://image.tmdb.org/t/p/w45/";
 export const VIDEO_LINK = "https://api.themoviedb.org/3/movie/";
-export const LocalhostClient = 'http://localhost:3000/';
+export const LocalhostClient = 'https://chomfilm.netlify.app/';
 export const LocalhostApi = 'http://localhost:5000/';
